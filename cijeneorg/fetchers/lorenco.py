c3ff81427421e546faa477e96401884f5a773dcd
IamMusavaRibica <65848142+IamMusavaRibica@users.noreply.github.com>
2025-10-25 22:10:02 +0200

    lorenco hotfix 3

--- cijeneorg/fetchers/lorenco.py
+++ cijeneorg/fetchers/lorenco.py
@@ -11,12 +11,15 @@ def fetch_lorenco_prices(lorenco: Store, min_date: date):
     for a in xpath(index_url, '//a[contains(@href, ".csv")]'):
         href = a.get('href')
         filename = href.split('/')[-1]
-        dt_str = (a.text.removeprefix('Cijenik')
+        dt_str = (a.text.removeprefix('_')
+                  .removeprefix('Cijenik')
                   .removeprefix('Cjenik')
                   .removeprefix('Cijene')
-                  .removeprefix('_')
                   .removesuffix('2024.')
                   .lstrip())
+        if dt_str == '.2025.': continue
+        if dt_str == '30.09':
+            dt_str += '.2025.'
         if dt_str.endswith('2025'):
             dt_str += '.'
         if not dt_str.endswith('2025.'):
